d5a85789a2ae75b395c32ced70d225579520c22b
lanou3g <lanou3g@lanou3gdeMac-mini.local>
2015-07-25 08:54:56 +0800

    Initial Commit

---
+++ WeiBo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeiBo
+//
+//  Created by lanou3g on 15/7/25.
+//  Copyright (c) 2015年 Lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
